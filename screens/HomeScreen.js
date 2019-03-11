c5ebba1c763fac968ca8adeb1507b180122ebbd9
satyamraz <42303218+satyamraz@users.noreply.github.com>
2019-03-11 13:16:44 +0530

    Add files via upload

---
+++ screens/HomeScreen.js
@@ -0,0 +1,114 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  TextInput
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+  state = {
+    email: '',
+    password: ''
+ }
+ handleEmail = (text) => {
+    this.setState({ email: text })
+ }
+ handlePassword = (text) => {
+    this.setState({ password: text })
+ }
+ login = (email, pass) => {
+    alert('email: ' + email + ' password: ' + pass)
+ }
+  render() {
+    return (
+      <View style = {styles.container}>
+         <TextInput style = {styles.input}
+            underlineColorAndroid = "transparent"
+            placeholder = "Email"
+            placeholderTextColor = "#9a73ef"
+            autoCapitalize = "none"
+            onChangeText = {this.handleEmail}/>
+         
+         <TextInput style = {styles.input}
+            underlineColorAndroid = "transparent"
+            placeholder = "Password"
+            placeholderTextColor = "#9a73ef"
+            autoCapitalize = "none"
+            onChangeText = {this.handlePassword}/>
+         
+         <TouchableOpacity
+            style = {styles.submitButton}
+            onPress = {
+               () => this.login(this.state.email, this.state.password)
+            }>
+            <Text style = {styles.submitButtonText}> Submit </Text>
+         </TouchableOpacity>
+      </View>
+   )
+  }
+
+  _maybeRenderDevelopmentModeWarning() {
+    if (__DEV__) {
+      const learnMoreButton = (
+        <Text onPress={this._handleLearnMorePress} style={styles.helpLinkText}>
+          Learn more
+        </Text>
+      );
+
+      return (
+        <Text style={styles.developmentModeText}>
+          Development mode is enabled, your app will be slower but you can use useful development
+          tools. {learnMoreButton}
+        </Text>
+      );
+    } else {
+      return (
+        <Text style={styles.developmentModeText}>
+          You are not in development mode, your app will run at full speed.
+        </Text>
+      );
+    }
+  }
+
+  _handleLearnMorePress = () => {
+    WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/guides/development-mode');
+  };
+
+  _handleHelpPress = () => {
+    WebBrowser.openBrowserAsync(
+      'https://docs.expo.io/versions/latest/guides/up-and-running.html#can-t-see-your-changes'
+    );
+  };
+}
+
+const styles = StyleSheet.create({
+  container: {
+     paddingTop: 23
+  },
+  input: {
+     margin: 15,
+     height: 40,
+     borderColor: '#7a42f4',
+     borderWidth: 1
+  },
+  submitButton: {
+     backgroundColor: '#7a42f4',
+     padding: 10,
+     margin: 15,
+     height: 40,
+  },
+  submitButtonText:{
+     color: 'white'
+  }
+})
